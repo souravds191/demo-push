87a63d6310bb41e7dfa992365a8e0969ee2dc9ab
souravds191 <souravds191@gmail.com>
2022-09-20 15:24:54 +0530

    changes made in index file

---
+++ Starter/main.js
@@ -0,0 +1,13 @@
+console.log(document.getElementById('header-title'));
+var headerTitle=document.getElementById('header-title');
+var header=document.getElementById('main-header');
+console.log(headerTitle);
+//headerTitle.textContent = 'Hello';
+//headerTitle.innerText = 'Goodbye';
+console.log(headerTitle.innerText);
+headerTitle.style.color='blue';
+header.style.border= 'solid 10px #000';
+var title=document.getElementById('main');
+
+ 
+
