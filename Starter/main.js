23aa9470ee98c639b88432b12ab2572d3c3327ce
souravds191 <souravds191@gmail.com>
2022-09-20 15:48:39 +0530

    font weight made bold

--- Starter/main.js
+++ Starter/main.js
@@ -8,6 +8,13 @@ console.log(headerTitle.innerText);
 headerTitle.style.color='blue';
 header.style.border= 'solid 10px #000';
 var title=document.getElementById('main');
+var items =document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[2].style.backgroundColor ='green';
+items[2].style.color ='white';
+// items.style.fontWeight='bold';//Doesn't Work
 
- 
-
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
